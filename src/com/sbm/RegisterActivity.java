00f20c44f4d8c77ecc80160b60fcb4e8efdaf9b4
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-10-02 00:03:29 -0400

    Implementing the Register screen allowing users to create account. This or login screen does not maitain a session. Will have to store the session later.

--- src/com/sbm/RegisterActivity.java
+++ src/com/sbm/RegisterActivity.java
@@ -1,32 +1,42 @@
 package com.sbm;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
-import android.content.SharedPreferences;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
 
-public class RegisterActivity extends Activity implements View.OnClickListener{
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
 
-    static final String TAG = "REGISTER";
+import static com.sbm.Global.*;
 
+public class RegisterActivity extends Activity implements View.OnClickListener, DataReceiver {
+
+    private Context context;
     private EditText editTextFirstName;
     private EditText editTextLastName;
-    private EditText editTextUsername;
+    private EditText editTextEmail;
     private EditText editTextPassword;
     private EditText editTextPasswordConfirmation;
     private Button buttonSubmit;
-    public static String firstname = null;
-    public static String lastname = null;
-    public static String username = null;
-    public static String password = null;
-    public static String passwordconfirmation = null;
-    public SharedPreferences preferences;
-    private Context context;
+
+    private String[] params = new String[5];
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,7 +46,7 @@ public class RegisterActivity extends Activity implements View.OnClickListener{
 
         editTextFirstName = (EditText) findViewById(R.id.editTextFirstName);
         editTextLastName = (EditText) findViewById(R.id.editTextLastName);
-        editTextUsername = (EditText) findViewById(R.id.editTextUsername);
+        editTextEmail = (EditText) findViewById(R.id.editTextRegisterEmail);
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
         editTextPasswordConfirmation = (EditText) findViewById(R.id.editTextConfirmPassword);
         buttonSubmit = (Button) findViewById(R.id.buttonSubmit);
@@ -46,28 +56,92 @@ public class RegisterActivity extends Activity implements View.OnClickListener{
 
     @Override
     public void onClick(View v) {
-//        switch (v.getId()) {
-//            case R.id.buttonSubmit:
-//                firstname = editTextFirstName.getText().toString();
-//                lastname = editTextLastName.getText().toString();
-//                username = editTextUsername.getText().toString();
-//                password = editTextPassword.getText().toString();
-//                passwordconfirmation = editTextPasswordConfirmation.getText().toString();
-//                Toast.makeText(context, "Submitted", Toast.LENGTH_SHORT);
-//                break;
-//        }
-    	if (v.getId() == R.id.buttonSubmit) {
-            firstname = editTextFirstName.getText().toString();
-            lastname = editTextLastName.getText().toString();
-            username = editTextUsername.getText().toString();
-            password = editTextPassword.getText().toString();
-            passwordconfirmation = editTextPasswordConfirmation.getText().toString();
-            Toast.makeText(context, "Submitted", Toast.LENGTH_SHORT).show();
-    	}
+        switch (v.getId()) {
+            case R.id.buttonSubmit:
+                params[0] = editTextFirstName.getText().toString();
+                params[1] = editTextLastName.getText().toString();
+                params[2] = editTextEmail.getText().toString();
+                params[3] = editTextPassword.getText().toString();
+                params[4] = editTextPasswordConfirmation.getText().toString();
+                RegisterSession session = new RegisterSession(context);
+                session.delegate = (DataReceiver) context;
+                session.execute(params);
+                break;
+        }
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
+    public void receive(ServerResponse response) {
+        if (response != null) {
+            if (response.getStatusCode() == HTTP_CREATED) {
+                startActivity(new Intent(context, MainActivity.class));
+                finish();
+            } else {
+                Toast.makeText(context, response.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
     }
+
+    private class RegisterSession extends AsyncTask<String, Integer, ServerResponse> {
+        private final Context RegisterSessionContext;
+        private ProgressDialog dialog;
+        public DataReceiver delegate;
+
+        public RegisterSession(Context context) {
+            RegisterSessionContext = context;
+            dialog = new ProgressDialog(RegisterSessionContext);
+        }
+
+        protected void onPreExecute() {
+            super.onPreExecute();
+            this.dialog.setMessage("Signing up..");
+            this.dialog.show();
+        }
+
+        @Override
+        protected ServerResponse doInBackground(String... params) {
+            ServerResponse serverResponse = null;
+
+            HttpClient client = new DefaultHttpClient();
+            HttpPost post = new HttpPost(REGISTER_URL);
+
+            List<NameValuePair> value = new LinkedList<NameValuePair>();
+            value.add(new BasicNameValuePair(FIRST_NAME, params[0]));
+            value.add(new BasicNameValuePair(LAST_NAME, params[1]));
+            value.add(new BasicNameValuePair(EMAIL, params[2]));
+            value.add(new BasicNameValuePair(PASSWORD, params[3]));
+            value.add(new BasicNameValuePair(PASSWORD_CONFIRMATION, params[4]));
+
+            try {
+                post.setEntity(new UrlEncodedFormEntity(value));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            try {
+                HttpResponse httpResponse = client.execute(post);
+                BufferedReader reader = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+                String responseString = reader.readLine();
+                serverResponse = new ServerResponse(httpResponse.getStatusLine().getStatusCode(), responseString);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return serverResponse;
+        }
+
+        @Override
+        protected void onPostExecute(ServerResponse response) {
+            super.onPostExecute(response);
+            delegate.receive(response);
+            if (dialog.isShowing())
+                dialog.dismiss();
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+    }
+
 }
