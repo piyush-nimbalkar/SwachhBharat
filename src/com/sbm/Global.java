935d2bc7571306d719076e024a6d139f8e9d1d61
Piyush Nimbalkar <piyushmnimbalkar@gmail.com>
2014-10-01 23:59:29 -0400

    Adding some constants to be user in login and register activities

--- src/com/sbm/Global.java
+++ src/com/sbm/Global.java
@@ -2,8 +2,16 @@ package com.sbm;
 
 public final class Global {
 
-    static final String REGISTER_URL = "http://mpss.csce.uark.edu/~devan/register.php";
+    static final String REGISTER_URL = "http://swachh-bharat.herokuapp.com/users";
     static final String LOGIN_URL = "http://swachh-bharat.herokuapp.com/login";
-    static final String USERNAME = "username";
+
+    static final String FIRST_NAME = "first_name";
+    static final String LAST_NAME = "last_name";
+    static final String EMAIL = "email";
+    static final String PASSWORD = "password";
+    static final String PASSWORD_CONFIRMATION = "password_confirmation";
+
+    static final int HTTP_SUCCESS = 200;
+    static final int HTTP_CREATED = 201;
 
 }
